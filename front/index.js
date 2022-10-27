a625b027492cf72c76ca4299d1d8d0b27e6b2e4f
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-27 18:08:50 +0200

    ADD, body added

--- front/index.js
+++ front/index.js
@@ -11,15 +11,24 @@ const paint = (movies) => {
   for (const movie of movies) {
     movies$$.innerHTML = "";
     const movie$$ = document.createElement("div");
-    const name$$ = document.createElement("h3");
+    const title$$ = document.createElement("h3");
     const img$$ = document.createElement("img");
+    const description$$ = document.createElement(".description");
+    const year$$ = document.createElement(".year");
+    const director$$ = document.createElement(".director");
 
     movie$$.classMovie.add("movie");
-    name$$.className = movie.name;
+    title$$.className = movie.title;
     img$$.classImg = movie.img;
+    description$$.className = movie.description;
+    year$$.className = movie.year;
+    director$$.className = movie.director;
 
-    movie$$.appendChild(name$$);
+    movie$$.appendChild(title$$);
     movie$$.appendChild(img$$);
+    movie$$.appendChild(description$$);
+    movie$$.appendChild(year$$);
+    movie$$.appendChild(director$$);
     movies$$.appendChild(movie$$);
   }
 };
